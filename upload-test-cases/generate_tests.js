d6e845c0549f82a356b3826caf2cfddb69ed25ad
Severyn Kozak <severyn.kozak@gmail.com>
2015-03-05 13:36:37 -0500

    Add documentation comments to generate_tests.js.
    upload-test-cases/generate_tests.js
    	-Add documentation comments to the module functions and header.

--- upload-test-cases/generate_tests.js
+++ upload-test-cases/generate_tests.js
@@ -1,9 +1,20 @@
+/**
+ * Given a path to a `pelias/acceptance-tests` tests JSON file, add the tests
+ * in the local Mongo server's Pelias collection (as stored by the feedback
+ * app) to it.
+ */
+
 var fs = require( 'fs' );
 var git = require( 'nodegit' );
 var mongo = require( 'mongodb' );
 var monk = require( 'monk' );
 var db = monk( 'localhost:27017/pelias' );
 
+/**
+ * Extract tests-cases from Mongo, conform them to the
+ * `pelias/acceptance-tests` format, inject them into the `testsJson` object,
+ * and pass it to `cb()`.
+ */
 function addTests( testsJson, cb ){
   var peliasCollection = db.get( 'pelias' );
 
@@ -39,6 +50,10 @@ function addTests( testsJson, cb ){
   });
 }
 
+/**
+ * Modify the JSON tests file specified by `testFilePath`, adding test cases to
+ * its `tests` property.
+ */
 function updateTestFile( testFilePath ){
   var testsJson = JSON.parse(fs.readFileSync( testFilePath ));
   addTests( testsJson, function writeNewFile( newTestJson ){
