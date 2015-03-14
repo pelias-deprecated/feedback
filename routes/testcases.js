9f8461931eadfb0d6102af3ed27c7c709cd5f2f3
Harish Krishna <harish.krsn@gmail.com>
2015-03-13 18:13:26 -0700

    fixes #6 test case coloring revamp

--- routes/testcases.js
+++ routes/testcases.js
@@ -9,11 +9,33 @@ router.get('/', function(req, res) {
     docs = docs.map(function(doc) {
       if (!doc.found) {
         doc.classname = 'danger';
-      } else {
-        doc.classname = !doc.nominatum_results ? 'success' : (doc.nominatum_results.length>0 ? 'warning' : 'danger');  
-      }
+      } 
       doc.found_in_pelias = doc.found && !doc.nominatum_results;
       doc.found_in_nominatum = doc.found && doc.nominatum_results && doc.nominatum_results.length>0;
+
+      if (!doc.found_in_pelias && doc.found_in_nominatum) {
+        doc.classname = 'danger';
+      } 
+
+      if (doc.found_in_pelias) {
+        doc.selected.map(function(d) {
+          var id = d.properties.id;
+          doc.results.forEach(function(dd, index){
+            if (dd.properties.id === id) {
+              doc.selected_index = doc.selected_index ? Math.min(doc.selected_index,index) : index;
+            }
+          });
+        });
+        
+        if(doc.selected_index>=0 && doc.selected_index<3) {
+          // if found in pelias and is the top 3 result
+          doc.classname = 'success';  
+        } else {
+          // if found in pelias and is not in the top 3
+          doc.classname = 'warning';
+        }
+      }
+
       if (doc.found_in_nominatum === undefined) {
         doc.found_in_nominatum = '-';
       }
